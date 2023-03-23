40dc76de56ac484d7e448b0e3ad4f0282712cde7
haifengzeng <3270937741@qq.com>
2023-03-23 10:02:18 +0800

    devser配置

--- webpack.config.js
+++ webpack.config.js
@@ -228,13 +228,28 @@ module.exports = {
         //启动gzip压缩
         compress:true,
         //端口号
-        port:3000,
+        port:5000,
+        //域名
+        host:'localhost',
         //自动打开浏览器
         open:true,
         //open: true,
         // 开启HMR功能
         // 当修改了webpack配置，新配置要想生效，必须重新webpack服务
-        hot:true
+        hot:true,
+        // 不要显示启动服务器日志信息
+        clientLogLevel: 'none',
+        // 服务器代理 --> 解决开发环境跨域问题
+        proxy:{
+            // 一旦devServer(5000)服务器接受到 /api/xxx 的请求，就会把请求转发到另外一个服务器(3000)
+            '/api':{
+                target:'http://localhost:3000',
+                // 发送请求时，请求路径重写：将 /api/xxx --> /xxx （去掉/api）
+                pathRewrite:{
+                    '^/api':''
+                }
+            }
+        }
     },
     optimization: {
         splitChunks: {
